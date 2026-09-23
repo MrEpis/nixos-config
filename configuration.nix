a6c1c18da548d9dba02747203e02c83d7b5c3bec
MrEpis <jesaispas2006@gmail.com>
2026-09-23 10:28:01 +0200

    fix

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # Réseau & Audio
   networking.networkmanager.enable = true;
